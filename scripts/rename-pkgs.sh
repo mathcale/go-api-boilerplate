2e248395e373d836e7383e5f426d17f3c0c258ab
Matheus Calegaro <hello@matheus.me>
2025-02-27 00:03:33 -0300

    fix(scripts): rename places using project name without org

--- scripts/rename-pkgs.sh
+++ scripts/rename-pkgs.sh
@@ -3,3 +3,4 @@
 read -p "==> Enter your project's name (e.g. mathcale/go-api-boilerplate): " -r PROJECT_NAME
 
 grep --exclude=rename-pkgs.sh --exclude-dir=.git "mathcale/go-api-boilerplate" . -lr | xargs sed -i "s|mathcale/go-api-boilerplate|$PROJECT_NAME|g"
+grep --exclude=rename-pkgs.sh --exclude-dir=.git "go-api-boilerplate" . -lr | xargs sed -i "s|go-api-boilerplate|${PROJECT_NAME#*/}|g"
